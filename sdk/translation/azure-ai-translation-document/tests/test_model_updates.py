ee764a403b4d1a16e1486ea1137ca10f8a5154e7
Hamshavathi Munibyraiah <v-hamunibyra@microsoft.com>
2024-10-09 13:32:48 -0700

    Adding a new test case for getSupportedFormats

--- sdk/translation/azure-ai-translation-document/tests/test_model_updates.py
+++ sdk/translation/azure-ai-translation-document/tests/test_model_updates.py
@@ -4,6 +4,7 @@
 # ------------------------------------
 
 import functools
+from azure.ai.translation.document.models import FileFormatType
 from testcase import DocumentTranslationTest
 from preparer import (
     DocumentTranslationPreparer,
@@ -24,4 +25,31 @@ class TestModelUpdates(DocumentTranslationTest):
         docs_count = 2
         self._prepare_and_validate_start_translation_details(client, docs_count, wait=False, variables=variables)
         return variables
+    
+    @DocumentTranslationPreparer()
+    @DocumentTranslationClientPreparer()
+    @recorded_by_proxy
+    def test_supported_formats(self, **kwargs):
+        client = kwargs.pop("client")
+        
+        # get supported glossary formats
+        supported_glossary_formats = client.get_supported_formats(FileFormatType.GLOSSARY)
+        assert supported_glossary_formats is not None
+        # validate
+        for glossary_format in supported_glossary_formats:
+            self._validate_format(glossary_format)
+
+        # get supported document formats
+        supported_document_formats = client.get_supported_formats(FileFormatType.DOCUMENT)
+        assert supported_document_formats is not None
+        # validate
+        for document_format in supported_document_formats:
+            self._validate_format(document_format)
+
+        # get supported formats
+        supported_formats = client.get_supported_formats()
+        assert supported_formats is not None
+        # validate
+        for format in supported_formats:
+            self._validate_format(format)
     
